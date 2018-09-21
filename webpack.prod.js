cc879e6329a3507705edde2dc6756ee6a6351438
Huynh Ha <huynhsamha@gmail.com>
2018-09-21 13:20:31 +0700

    test path

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,16 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const dist = path.resolve(__dirname, 'dist');
+
+// const name = require('./package.json').name; // '/webpack-setup-multiple-pages'
+
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  output: {
+    publicPath: '', // add prefix path when include
+    path: dist,
+    filename: 'js/[name].bundle.js' // in directory ./dist/js/*
+  }
 });
